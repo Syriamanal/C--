ec9eed3b1446a7f6429b3b2bc02acb27e9808a69
Syriamanal <Shadinachat123@gmail.com>
2013-12-25 12:28:42 -0800

    Fixed major bug

--- C++.cpp
+++ C++.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
-
-                        //Age tester!!!
+//Age tester!!!
 using namespace std;
-
-void age()
-
-if(age == 18)
-{
-cout << "You are a teenager";
-}
-else if(age == 17)
-{
-cout << "One more year to be 18!";
-}
-else if(age == 14)
-{
-cout << "You are in high school!";
-}
 
 
 int main()
 {
 int age;
-cout << "Enter Your age...";
+
+cout << "Enter Your age..." << endl;
 cin >> age;
-Cout << "You have entered" << age <<endl;
+cout << "You have enterd \n" << age << endl;
+
+
+if(age == 18)
+{
+cout << "You are a teenager" << endl;
+}
+else if(age == 17)
+{
+cout << "One more year to be 18!" << endl;
+}
+else if(age == 14)
+{
+cout << "You are in high school!" << endl;
+}
+else
+{
+cout << "Your to young!" << endl;
+}
 }
