171860e46b4d1fd694f1d3db9b248ad3ffb930d7
Syriamanal <Shadinachat123@gmail.com>
2013-12-25 12:15:14 -0800

    Create C++.cpp

---
+++ C++.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+                        //Age tester!!!
+using namespace std;
+
+void age()
+
+if(age == 18)
+{
+cout << "You are a teenager";
+}
+else if(age == 17)
+{
+cout << "One more year to be 18!";
+}
+else if(age == 14)
+{
+cout << "You are in high school!";
+}
+
+
+int main()
+{
+int age;
+cout << "Enter Your age...";
+cin >> age;
+Cout << "You have entered" << age <<endl;
+}
