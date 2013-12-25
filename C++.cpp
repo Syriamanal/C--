c128c2621cbe31dad5e20703b546302a8bbef967
Syriamanal <Shadinachat123@gmail.com>
2013-12-25 13:13:58 -0800

    Added String suport

--- C++.cpp
+++ C++.cpp
@@ -1,22 +1,29 @@
 #include <iostream>
-//Age tester!!!
+#include <string>
 using namespace std;
 
 
 int main()
 {
+    
 int age;
 
+//This will ask you to enter your age...
 cout << "Enter Your age..." << endl;
     cout << endl;
 cin >> age;
 cout << "You have entered  " << age << endl;
 
-
+//This will be the output...
 if(age == 18)
 {
     cout << endl;
     cout << "You are a teenager" << endl;
+}
+else if(age >= 18)
+{
+    cout << endl;
+    cout << "You are older than 18!"; 
 }
 else if(age == 17)
 {
@@ -30,7 +37,13 @@ else if(age == 14)
 }
 else
 {
+    //This will procec when age is bellow 14...
     cout << endl;
     cout << "Your to young!" << endl;
 }
+
+// String 
+string AgeString;
+    AgeString = "You need to do your homework!";
+    cout << AgeString <<endl;
 }
